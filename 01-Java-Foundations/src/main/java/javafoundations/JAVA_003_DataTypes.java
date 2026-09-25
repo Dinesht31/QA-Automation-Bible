a3c2ed62c813c38e89491669ad0d07cc363bdc5d
Dinesh <dinesht31@gmail.com>
2026-09-25 20:55:45 +0530

    JAVA-003: Add Java data types program and debugger exercise

---
+++ 01-Java-Foundations/src/main/java/javafoundations/JAVA_003_DataTypes.java
@@ -0,0 +1,28 @@
+package javafoundations;
+
+public class JAVA_003_DataTypes {
+    public static void main(String[]args){
+
+        int userId = 101;
+
+        String userName = "Dinesh";
+
+        int age = 41;
+
+        double walletBalance = 2575.50;
+
+        boolean emailVerified = true;
+
+        char gender = 'M';
+
+        String mobileNumber ="0987654321";
+
+        System.out.println(userId);
+        System.out.println(userName);
+        System.out.println(age);
+        System.out.println(walletBalance);
+        System.out.println(emailVerified);
+        System.out.println(gender);
+        System.out.println(mobileNumber);
+    }
+}
